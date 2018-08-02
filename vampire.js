a20bbd28a1109bf18edd70f160d285f85d54413f
Jaron D Evans <jarondevans@gmail.com>
2018-08-02 20:02:26 +0000

    completed

--- vampire.js
+++ vampire.js
@@ -6,27 +6,41 @@ class Vampire {
     this.creator = null;
   }
 
-  /** Simple tree methods **/
+
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
-  }
+    this.offspring.push(vampire);
+    vampire.creator = this;
+}
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
+
+    // climb "up" the tree (using iteration), counting nodes, until no boss is found
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+   if(this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal){
+      return true;
   }
+  return false;
+}
 
   /** Stretch **/
 
@@ -36,9 +50,19 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    return this.creator === vampire.creator;
 
   }
 }
+
+
+// const ada = new Vampire("Ada", "1990", null , null);
+// const craig    = new Vampire("Craig", "1995", ada);
+// const arvinder = new Vampire("Arvinder", "1995", ada);
+// const angela   = new Vampire("Angela", "1995", ada);
+// const phil     = new Vampire("Phil", "1995", ada);
+
+
 
 module.exports = Vampire;
 
